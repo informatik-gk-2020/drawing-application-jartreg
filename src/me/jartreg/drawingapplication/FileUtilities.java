82160a769c69ec6b3ed2ae0b4294d6feb5a2532a
Jartreg <jartreg@users.noreply.github.com>
2018-11-02 12:38:45 +0100

    Add comments to `FileUtilities.java`

--- src/me/jartreg/drawingapplication/FileUtilities.java
+++ src/me/jartreg/drawingapplication/FileUtilities.java
@@ -9,36 +9,58 @@ import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ForkJoinPool;
 
+/**
+ * Eine Hilfeklasse zum Speichern und Öffnen von Bildern
+ */
 public final class FileUtilities {
     private FileUtilities() {
     }
 
+    /**
+     * Speichert ein Bild in einer Datei
+     *
+     * @param image das Bild
+     * @param file  die Datei
+     * @return ob das Speichern erfolgreich war
+     */
     public static boolean save(BufferedImage image, File file) {
-        var result = new boolean[]{false};
+        var result = new boolean[]{false}; // wird enthalten, ob erfolgreich gespeichert wurde
+
+        // Wird benutzt, um das Programm zu pausieren, wärend das Bild gespeichert wird
         var eventQueue = Toolkit.getDefaultToolkit().getSystemEventQueue().createSecondaryLoop();
 
+        // Speichert das Bild in einem anderen Thread, damit die Anwendung nicht hängt
         ForkJoinPool.commonPool().execute(() -> {
             try {
-                ImageIO.write(image, "PNG", file);
-                result[0] = true;
+                ImageIO.write(image, "PNG", file); // speichern
+                result[0] = true; // war erfolgreich
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            eventQueue.exit();
+            eventQueue.exit(); // Setzt das Programm fort (eventQueue.enter() wird weiterlaufen)
         });
 
-        eventQueue.enter();
+        eventQueue.enter(); // Pausiert das Programm
+        // Wenn dieser Punkt erreicht wurde, wurde das Bild bereits gespeichert
         return result[0];
     }
 
+    /**
+     * Öffnet ein Bild aus einer Datei
+     * @param file die Datei
+     * @return das Bild
+     */
     public static BufferedImage open(File file) {
-        var result = new BufferedImage[]{null};
+        var result = new BufferedImage[]{null}; // Wird das Bild enthalten
+
+        // Wird benutzt, um das Programm zu pausieren, wärend das Bild gelesen wird
         var eventQueue = Toolkit.getDefaultToolkit().getSystemEventQueue().createSecondaryLoop();
 
+        // Öffnet das Bild in einem anderen Thread, damit die Anwendung nicht hängt
         ForkJoinPool.commonPool().execute(() -> {
             try {
-                result[0] = ImageIO.read(file);
+                result[0] = ImageIO.read(file); // lesen
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -46,32 +68,52 @@ public final class FileUtilities {
             eventQueue.exit();
         });
 
-        eventQueue.enter();
+        eventQueue.enter(); // Pausiert das Programm
+        // Wenn dieser Punkt erreicht wurde, wurde das Bild bereits gelesen
         return result[0];
     }
 
+    /**
+     * Zeigt einen "Speichern"-Dialog an
+     * @param parent das Fenster, über dem der Dialog angezeugt werden soll
+     * @return die ausgewählte Datei oder <code>null</code>
+     */
     public static File showSaveDialog(Frame parent) {
         var fileChooser = new JFileChooser();
+
+        // Nur .png-Dateien werden zugelassen
         fileChooser.setFileFilter(new FileNameExtensionFilter("PNG-Dateien", "png"));
 
+        // Wenn der Nutzer bestätigt hat...
         if (fileChooser.showSaveDialog(parent) == JFileChooser.APPROVE_OPTION) {
             var file = fileChooser.getSelectedFile();
+
+            // Wenn der Dateiname noch keine Endung enthält, wird eine angefügt
             if (!file.getName().toLowerCase().endsWith(".png"))
                 file = new File(file.getAbsolutePath() + ".png");
+
             return file;
         }
 
-        return null;
+        return null; // der Nutzer hat nicht bestätigt
     }
 
+    /**
+     * Zeigt einen "Öffnen"-Dialog an
+     * @param parent das Fenster, über dem der Dialog angezeugt werden soll
+     * @return die ausgewählte Datei oder <code>null</code>
+     */
     public static File showOpenDialog(Frame parent) {
         var fileChooser = new JFileChooser();
+
+        // Nur .png-Dateien werden zugelassen
         fileChooser.setFileFilter(new FileNameExtensionFilter("PNG-Dateien", "png"));
 
+        // Wenn der Nutzer bestätigt hat...
         if (fileChooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
             return fileChooser.getSelectedFile();
         }
 
-        return null;
+        return null; // der Nutzer hat nicht bestätigt
     }
 }
